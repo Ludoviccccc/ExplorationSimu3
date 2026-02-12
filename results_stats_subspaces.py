8fc9cdd953e5ca5fed656a0fee340da60a457578
Ludoviccccc <ludovic.matar@inria.fr>
2026-02-12 17:54:12 +0100

    diversity subspaces

---
+++ results_stats_subspaces.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append('../../')
+import pickle
+import os
+import numpy as np
+from scipy.special import stdtr, stdtrit
+from multiprocessing import Pool
+# This codes evaluates the diversity evolution of multiple IMGEP/RANDOM exploration runs
+# in parallel (using Pool) and determines asymptotic confidence intervals for each point of the curves
+
+def bin_diversity(content):
+    div = 5*np.ones(content.shape[1])
+    coords = (content)//div
+    c = np.unique(coords,axis=0)
+    return c
+
+def evaluate_hist_diversity(content_list):
+    step = 1000
+    n_func = 5
+    diversity_time = [0]+ [len(bin_diversity(content_list[:j,idx_time])) for j in range(0,len(content_list),step) if j!=0]+ [len(bin_diversity(content_list[:,idx_time]))]
+    diversity_remain = [0]+ [len(bin_diversity(content_list[:j,idx_remain])) for j in range(0,len(content_list),step) if j!=0]+ [len(bin_diversity(content_list[:,idx_remain]))]
+    return [diversity_time,diversity_remain]
+def Sn(diversity_array:np.ndarray):
+    """
+    computes the sigma estimator of the diversity values
+    """
+    mean_ = diversity_array.mean(axis=0)
+    out = np.sqrt(np.sum((diversity_array - mean_)**2,axis=0)/(diversity_array.shape[0]-1))
+    return out
+def CI(diversity_array,alpha=.05):
+    n = diversity_array.shape[0]
+    qt = stdtrit(n-1,1-alpha)
+    sig = Sn(diversity_array)
+    mean_ = diversity_array.mean(axis=0)
+    inf = mean_ - qt*sig*(1.0/np.sqrt(n))
+    sup = mean_ + qt*sig*(1.0/np.sqrt(n))
+    x_axis = 1000*np.arange(len(mean_))
+    return {'mean':mean_,'inf':inf,'sup':sup,'iterations':x_axis}
+
+if __name__=='__main__':
+    N = 10000
+    k_values = [1,2,3]
+    folder = 'results' 
+    algo_list = ['imgep','operators','rand']
+    CI_algo_time = {algo:{k:[] for k in k_values} for algo in algo_list}
+    CI_algo_remain = {algo:{k:[] for k in k_values} for algo in algo_list}
+    N = 10000
+    M = 20
+    time_var = ['mutual_diff_time_core0',
+                 'mutual_diff_time_core1',
+                 'mutual_diff_time']
+    j_list = range(M)
+    print('start opening files')
+    for algo in algo_list:
+        for k in k_values:
+            content_list = []
+            for j in j_list:
+                if algo in ['imgep','operators']:
+                    name = f'{algo}_run_{k}_{N}_{j}.pkl'
+                else:
+                    if k>1:
+                        break
+                    name = f'{algo}_run_{N}_{j}.pkl'
+                if j%100==0:
+                    print(f'opening {name}')
+                try:
+                    with open(os.path.join(folder,name),'rb') as f:
+                        stats = pickle.load(f)
+                        content_list.append(stats['tabular_view'])
+                        idx_time = np.array([j for j in range(len(stats['names'])) if stats['names'][j] in time_var])
+                        idx_remain = np.array([j for j in range(len(stats['names'])) if not (stats['names'][j] not in time_var)])
+                except:
+                    print(f'fail at opening {name}')
+
+            diversity_list = []
+            n_func = 10
+            for j in range(len(content_list)//n_func):
+                with Pool(40) as p: 
+                    batch_div = p.map(evaluate_hist_diversity,content_list[j*n_func:(j+1)*n_func])
+                diversity_list+=batch_div
+            diversity_list = np.array(diversity_list)
+            diversity_array_time = diversity_list[:,0,:]
+            diversity_array_remain = diversity_list[:,1,:]
+            print('diversity_array_time', diversity_array_time.shape,algo,f'k={k}')
+            print('diversity_array_remain', diversity_array_remain.shape,algo,f'k={k}')
+            CI_algo_time[algo][k] = CI(diversity_array_time)
+            CI_algo_remain[algo][k] = CI(diversity_array_remain)
+    with open('ci_diversity_time.pkl','wb') as f:
+        pickle.dump(CI_algo_time,f)
+    print('dumped!')
+    with open('ci_diversity_remain.pkl','wb') as f:
+        pickle.dump(CI_algo_remain,f)
+    print('dumped!')
