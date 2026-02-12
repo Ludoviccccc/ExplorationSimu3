3123f0c1a1dc9fadcc8ef9118032680288c5ef10
Ludoviccccc <ludovic.matar@inria.fr>
2026-02-12 18:22:28 +0100

    visualize asymptotic confidence intervals

---
+++ visualisation/visu_CI.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import pickle
+import sys
+sys.path.append('../')
+
+def visu_div_iterations(data,title=None):
+    k_values = [1,2,3]
+    plt.figure()
+    for algo in ['imgep', 'operators','rand']:
+        for k in k_values:
+            if algo=='rand':
+                if k>1:
+                    break
+            x = data[algo][k]['iterations']
+            mean_ = data[algo][k]['mean']
+            inf = data[algo][k]['inf']
+            sup = data[algo][k]['sup']
+            if algo=='rand':
+                plt.plot(x,mean_,'-o',label=f'mean value {algo}')
+            else:
+                plt.plot(x,mean_,'-o',label=f'mean value {algo}, k={k}')
+            plt.fill_between(x, inf, sup,  alpha=.1)
+            plt.legend()
+    if title:
+        plt.title(title+' mean diversity, 95 % asymptotic CI interval, 500 runs')
+    plt.xlabel('iterations')
+    plt.ylabel('diversity: nb of bins filled')
+    plt.grid()
+if __name__=='__main__':
+    with open('ci_diversity.pkl','rb') as f:
+        data = pickle.load(f)
+    visu_div_iterations(data,title='entire space')
+    plt.savefig('entire_space_div.png')
+    plt.show()
+
+    with open('ci_diversity_time.pkl','rb') as f:
+        data = pickle.load(f)
+    print(data['imgep'][1].keys())
+    visu_div_iterations(data,title='Time subspace')
+    plt.savefig('time_space_div.png')
+    plt.show()
+
+    with open('ci_diversity_remain.pkl','rb') as f:
+        data = pickle.load(f)
+    visu_div_iterations(data,title='Miss and hit subspace')
+    plt.savefig('miss_hit_space_div.png')
+    plt.show()
